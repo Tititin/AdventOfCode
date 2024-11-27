b5217ae1da534ffc76a20b10db51bb3ae2082146
Val'VR <valvrpro@gmail.com>
2024-11-27 14:53:38 +0100

    Delete AdventOfCode/2022 directory on 2015 branch

--- AdventOfCode/2022/DaySix.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "FileParser.h"
-#include <list>
-
-bool checkMarkerValidity(const std::list<char>& marker, const int& requiredLength);
-
-void daySix(bool isPartTwo = false);
