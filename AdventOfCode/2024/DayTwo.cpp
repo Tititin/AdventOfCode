3882a8f30d66424bb3c2b59b593fb3f702b2b49a
Val'VR <valvrpro@gmail.com>
2024-12-01 11:08:58 +0100

    Preparing DayTwo

---
+++ AdventOfCode/2024/DayTwo.cpp
@@ -0,0 +1,13 @@
+#include "DayTwo.h"
+
+void dayTwo(const bool& isPartTwo)
+{
+	FileParser	_file("2024\\InputFiles\\inputD2.txt");
+	std::string	line;
+	unsigned long long int	finalValue = 0;
+
+	while ((line = _file.readLineToString()) != "")
+	{
+
+	}
+}
